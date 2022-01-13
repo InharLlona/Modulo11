cb99f48a43e91f2467d0d1c43ebb8818c34488bd
InharLlona <inharllona@gmail.com>
2022-01-13 10:30:15 +0100

    ok

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const reservas = [
     {
       tipoHabitacion: "standard",
       desayuno : true,
-      pax: 1,
+      pax: 2,
       noches: 1
     },
     {
@@ -61,10 +61,10 @@ const reservas = [
 
 
   const precio1 = new precio (reservas);
-  console.log("Al ser un PARTICULAR el Subtotal asciende a ",precio1.Subtotal,"€ y el Toatal con IVA del 21% asciende a ",precio1.Total,"€" )
+  console.log("Al ser un PARTICULAR el Subtotal asciende a ",precio1.Subtotal.toFixed(2),"€ y el Toatal con IVA del 21% asciende a ",precio1.Total.toFixed(2),"€" )
 
   const precio2 = new precioTour(reservas);
-  console.log("Al ser un OPERADOR el Subtotal asciende a ",precio2.Subtotal,"€ y el Toatal con IVA del 21% y un descuento del 10% asciende a ",precio2.Total,"€" )
+  console.log("Al ser un OPERADOR el Subtotal asciende a ",precio2.Subtotal.toFixed(2),"€ y el Toatal con IVA del 21% y un descuento del 10% asciende a ",precio2.Total.toFixed(2),"€" )
 
 //////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////// DESFIO
@@ -81,7 +81,7 @@ class bass {
     this.calcularTotal();
   }
   calcularSubtotal(pr1,pr2){
-    this.subtotal = this.reserva.reduce((acc,reser) => acc += (((reser.tipoHabitacion ==="standard")?(pr1*reser.noches+((reser.pax-1)*40)*reser.noches):pr2*reser.noches+((reser.pax-1)*40)*reser.noches) + ((reser.desayuno)? reser.noches*15:0)) ,0 )  
+    this.subtotal = this.reserva.reduce((acc,reser) => acc += (((reser.tipoHabitacion ==="standard")?(pr1*reser.noches+((reser.pax-1)*40)*reser.noches):pr2*reser.noches+((reser.pax-1)*40)*reser.noches) + ((reser.desayuno)? reser.noches*15*reser.pax:0)) ,0 )  
   }
   calcularTotal(ds){
     this.total = (this.subtotal + (this.subtotal * 0.21)) - ((this.subtotal + (this.subtotal * 0.21)) * ds); 
@@ -108,8 +108,8 @@ class child2 extends bass{
  const precioch2 = new child2 (price);
  precioch1.reservain = reservas;
  precioch2.reservain = reservas;
-  console.log("Al ser un PARTICULAR el Subtotal asciende a ",precioch1.subtotal,"€ y el Toatal con IVA del 21% asciende a ",precioch1.total.toFixed(2),"€" )
-  console.log("Al ser un OPERADOR el Subtotal asciende a ",precioch2.subtotal,"€ y el Toatal con IVA del 21% y un descuento del 10% asciende a ",precioch2.total.toFixed(2),"€" )
+  console.log("Al ser un PARTICULAR el Subtotal asciende a ",precioch1.subtotal.toFixed(2),"€ y el Toatal con IVA del 21% asciende a ",precioch1.total.toFixed(2),"€" )
+  console.log("Al ser un OPERADOR el Subtotal asciende a ",precioch2.subtotal.toFixed(2),"€ y el Toatal con IVA del 21% y un descuento del 10% asciende a ",precioch2.total.toFixed(2),"€" )
 
 //////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////// EJERCICIO EXTRA (añadir desayuno lo he puesto con el desafio)
